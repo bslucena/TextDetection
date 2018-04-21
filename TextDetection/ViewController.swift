8ca10173c5c8514470c395a7ad957b675db53d8c
Pernas <bslucena@gmail.com>
2018-04-21 19:17:35 +0200

    Initial Commit

---
+++ TextDetection/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TextDetection
+//
+//  Created by Bernardo Sarto de Lucena on 4/21/18.
+//  Copyright © 2018 Bernardo Sarto de Lucena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
